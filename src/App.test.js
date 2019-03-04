f8357021029ecb37f59dbe703ef197ea473483e1
ebinhon <bin.hong@ericsson.com>
2019-03-04 18:24:00 +0800

    add UT

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { mount } from 'enzyme'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const wrapper = mount(
+    <Router>
+      <App />
+    </Router>
+  )
+  const Link = wrapper.find('Link')
+  expect(Link.exists()).toBe(true)
+  expect(Link.text()).toBe('Users Page')
+})
