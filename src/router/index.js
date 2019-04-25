ca286027aebe32a76bc4918d5acac8d6c43d2d52
ebinhon <bin.hong@ericsson.com>
2019-04-25 17:09:00 +0800

    add content '/demo'

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,21 @@
  * @Author: ebinhon
  * @Date: 2019-03-04 14:41:00
  * @Last Modified by: ebinhon
- * @Last Modified time: 2019-03-04 17:47:55
+ * @Last Modified time: 2019-04-25 17:04:42
  */
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import App from '../App'
 import UsersPage from '../pages/users'
 
 const Routes = () => (
   <div>
-    <Switch>
-      <Route exact path='/' component={App} />
-      <Route path='/users' component={UsersPage} />
-    </Switch>
+    <BrowserRouter basename='/demo'>
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/users' component={UsersPage} />
+      </Switch>
+    </BrowserRouter>
   </div>
 )
 
